e811b3fa4e89671d0dd7cece0c52e7fe0f5d19c8
chobaa <81730406+chobaa@users.noreply.github.com>
2026-02-25 15:03:15 +0900

    강의: 7주차 수업까지 완료

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityManager;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.service.MemberService;
+import hello.hello_spring.app.TimeTraceAop;
 
 @Configuration
 public class SpringConfig {
@@ -19,22 +20,26 @@ public class SpringConfig {
     }
 
     /*
-    private final EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-    */
+     * private final EntityManager em;
+     * 
+     * @Autowired
+     * public SpringConfig(EntityManager em) {
+     * this.em = em;
+     * }
+     */
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
 
+    @Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }
     // @Bean
     // public MemberRepository memberRepository() {
-        // return new JdbcMemberRepository(dataSource);
-        // return new JpaMemberRepository(em);
+    // return new JdbcMemberRepository(dataSource);
+    // return new JpaMemberRepository(em);
     // }
 }
